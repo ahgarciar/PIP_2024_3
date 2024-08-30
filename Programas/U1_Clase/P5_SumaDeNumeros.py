4c840db7c07e7d1803f85c4899d44d59a2d2d0d0
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2024-08-30 09:57:45 -0600

    5

---
+++ Programas/U1_Clase/P5_SumaDeNumeros.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "P5_SumaDeNumeros.ui"  # Nombre del archivo aquí.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        # Área de los Signals (Evento)
+        try:
+            self.btn_sumar.clicked.connect(self.sumar)
+        except Exception as error:
+            print(error)
+
+    # Área de los Slots (Funciones)
+    def sumar(self):
+        numeros = self.txt_numeros.text()  # str "34+56+67"
+
+        resultado = eval(numeros)
+
+        #self.mensaje("La suma es: " + str(resultado))
+
+        self.txt_resultado.setText(str(resultado))
+
+    def mensaje(self, texto):
+        m = QtWidgets.QMessageBox()
+        m.setText(texto)
+        m.exec()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+
