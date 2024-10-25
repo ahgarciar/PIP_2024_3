6feaa50d6e88418f9ae4810bd04981540a726399
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2024-10-25 09:52:06 -0600

    update

--- Programas/U3_Clase/P4_TranspasoInfoVentanas/Ejemplo 1/Ejecuta_EnvioInformacion.py
+++ Programas/U3_Clase/P4_TranspasoInfoVentanas/Ejemplo 1/Ejecuta_EnvioInformacion.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import uic, QtWidgets, QtCore
 
+import VentanaSecundaria
 ##########################################################################
 
 qtCreatorFile1 = "Main_EnvioInfo.ui"  # Nombre del archivo aquí.
@@ -13,33 +14,20 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow1):
         self.setupUi(self)
         # Área de los Signals / Configuracion
         self.btn_sumar.clicked.connect(self.sumar)
+
     # Área de los Slots
     def sumar(self):
         a = int(self.txt_a.text())
         b = int(self.txt_b.text())
         r = a + b
-        self.dialogo = MyDialog()
-        self.dialogo.setModal(False)
+
+        self.dialogo = VentanaSecundaria.MyDialog()
+        self.dialogo.setModal(True)
         self.dialogo.txt_resultado.setText(str(r))
         self.dialogo.show()
 
 ##########################################################################
 
-qtCreatorFile3 = "Second_EnvioInfo.ui"  # Nombre del archivo aquí.
-Ui_dialog, QtBaseClass3 = uic.loadUiType(qtCreatorFile3)
-
-class MyDialog(QtWidgets.QDialog, Ui_dialog):
-    def __init__(self):
-        QtWidgets.QDialog.__init__(self)
-        Ui_dialog.__init__(self)
-        self.setupUi(self)
-
-        # Área de los Signals / Configuracion
-
-    # Área de los Slots
-
-
-##########################################################################
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- Programas/U3_Clase/P4_TranspasoInfoVentanas/Ejemplo 2/Ejecuta_RecepcionInformacion.py
+++ Programas/U3_Clase/P4_TranspasoInfoVentanas/Ejemplo 2/Ejecuta_RecepcionInformacion.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic, QtWidgets, QtCore
-
+import VentanaSecundaria
 ##########################################################################
 
 qtCreatorFile1 = "Main_RecepcionInfo.ui"  # Nombre del archivo aquí.
@@ -17,41 +17,14 @@ class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow1):
 
     # Área de los Slots
     def sumar(self):
-        self.dialogo = MyDialog(self)
+        # manda la referencia de la pantalla principal al dialog
+        self.dialogo = VentanaSecundaria.MyDialog(self)
+
         self.dialogo.setModal(True)
         self.dialogo.show()
 
 ##########################################################################
 
-qtCreatorFile3 = "Second_RecepcionInfo.ui"  # Nombre del archivo aquí.
-Ui_dialog, QtBaseClass3 = uic.loadUiType(qtCreatorFile3)
-
-
-class MyDialog(QtWidgets.QDialog, Ui_dialog):
-    def __init__(self,  rPrincipal):
-        QtWidgets.QDialog.__init__(self)
-        Ui_dialog.__init__(self)
-        self.setupUi(self)
-
-        # Área de los Signals / Configuracion
-        self.acceso = rPrincipal
-
-        self.btn_sumar.clicked.connect(self.sumar)
-
-
-    # Área de los Slots
-    def sumar(self):
-        a = int(self.txt_a.text())
-        b = int(self.txt_b.text())
-
-        r = a + b
-        print(r)
-
-        self.acceso.txt_resultado.setText(str(r))
-
-        self.close()
-
-##########################################################################
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
